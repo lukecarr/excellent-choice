2545c4f236bc0e1abca94bfa992093f5b0ec1ac2
Luke Carr <luke_jcarr@icloud.com>
2021-11-11 11:19:57 +0000

    ♻️ Refactored textual converter

--- src/textual.ts
+++ src/textual.ts
@@ -1,6 +1,22 @@
 import { Question } from './types'
 
 /**
+ * Converts a question into a H5P textual file.
+ * 
+ * @param question The question to convert.
+ * @returns The H5P textual file as a string.
+ */
+function question (question: Question): string {
+  let textual = `${question.stem}\n`
+
+  for (const alternative of question.alternatives) {
+    textual += `${alternative.correct ? '*' : ''}${alternative.text}\n`
+  }
+
+  return textual
+}
+
+/**
  * Converts questions into an H5P textual file.
  *
  * @param questions The questions to convert.
@@ -9,12 +25,8 @@ import { Question } from './types'
 export function textual (questions: Question[]): string {
   let textual = ''
 
-  for (const question of questions) {
-    textual += `${question.stem}\n`
-
-    for (const alternative of question.alternatives) {
-      textual += `${alternative.correct ? '*' : ''}${alternative.text}\n`
-    }
+  for (const q of questions) {
+    textual += question(q)
   }
 
   return textual
